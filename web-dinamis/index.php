0df8fb4b8a3608920443b4eb2965195b8073acd3
maemunah15 <maemunahmatus@gmail.com>
2026-06-08 13:06:34 +0700

    menggubah dasboard

--- web-dinamis/index.php
+++ web-dinamis/index.php
@@ -31,7 +31,7 @@ $success = $_GET['success'] ?? '';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dashboard — Akademik</title>
+  <title>Dashboard — Informatika</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
